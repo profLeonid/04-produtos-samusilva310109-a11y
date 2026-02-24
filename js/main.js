4c4d672cb7510c1643af47fd78b77e77d56c6247
SAMUEL SILVA MOREIRA DOS SANTOS <25203710@dsn127m1249389.cfp127aluno.local>
2026-02-24 16:59:32 -0300

    Produção de tabela em dessenvolvimento. Feito campos de inserção de dados

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 'use strict'
 
 function adicionarProduto(){
+    const codigo = document.getElementById('inp-codigo')
     const produto = document.getElementById('inp-produto')
+    const quantidade = document.getElementById('inp-quantidade')
     const lista = document.getElementById('list')
 
     const span = document.createElement('span')
-    if(produto.value != ''){
+    if(produto.value != '' && codigo.value != '' && quantidade.value != ''){
         span.textContent = produto.value
         span.className = "bg-blue-100 p-2"
-        produto.value = "" 
+        produto.value = '' 
+        codigo.value = ''
+        quantidade.value = ''
     
         lista.appendChild(span)
-    }else{
+        
+    }
+    else{
         window.alert('Preencha todos os campos!!')
     }
     
